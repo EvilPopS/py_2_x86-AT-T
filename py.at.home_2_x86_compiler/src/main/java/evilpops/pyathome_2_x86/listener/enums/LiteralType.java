b3d2a92a46ac3b51ec1880507f57e06c8bc436ae
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2023-11-10 15:04:39 +0100

    [#1] Removed unused enums from listener package

--- py.at.home_2_x86_compiler/src/main/java/evilpops/pyathome_2_x86/listener/enums/LiteralType.java
+++
@@ -1,6 +0,0 @@
-package main.java.evilpops.pyathome_2_x86.listener.enums;
-
-public enum LiteralType {
-    NUMBER,
-    BOOLEAN
-}
--- py.at.home_2_x86_compiler/src/main/java/evilpops/pyathome_2_x86/listener/enums/OperatorTypes.java
+++
@@ -1,5 +0,0 @@
-package main.java.evilpops.pyathome_2_x86.listener.enums;
-
-public enum OperatorTypes {
-    PLUS
-}
